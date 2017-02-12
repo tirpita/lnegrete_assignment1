6d8a0f88d386adc046c1b12d25b403e39fb96cb0
tirpita <flowbin100@yahoo.com>
2017-02-11 20:01:44 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//STEP 1
+var someMonth;
+function TheMonth 		// function to return current month
+currentMonth            // a constant 
+var summerMonth; 		// an array of summer months
+myLibraryAfunction	    // a function 
+
+//STEP 2
