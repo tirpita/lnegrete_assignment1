8a3effb0711e5bd480e39e62c7c55be6b4c406c6
tirpita <NEGRETE@leahs-imac.attlocal.net>
2017-02-13 20:58:49 -0800

    ass1 complete

--- javascript.js
+++
@@ -1,8 +0,0 @@
-//STEP 1
-var someMonth;
-function TheMonth 		// function to return current month
-currentMonth            // a constant 
-var summerMonth; 		// an array of summer months
-myLibraryAfunction	    // a function 
-
-//STEP 2
--- script.js
+++ script.js
@@ -1,8 +1,202 @@
 //STEP 1
+/*
 var someMonth;
-function TheMonth 		// function to return current month
-currentMonth            // a constant 
+fnctTheMonth 		// function to return current month
+constCurrentMonth            // a constant 
 var summerMonth; 		// an array of summer months
 myLibraryAfunction	    // a function 
+*/
 
 //STEP 2
+
+/*
+2017    // numeric literal 
+"This is a string." //string literal
+true    //boolean literal
+null //null literal
+*/
+
+//STEP 3
+
+/*
+var nine = 5 * 2 - 1
+var name = "firstName" + "lastName"
+*/
+
+//STEP 4
+
+/*
+var firstName;
+var lastName;
+var address;
+var city;
+var state; 
+var zipcode;
+var yourAge;
+var refferalSource;
+var mayWeContactYou;
+*/
+
+//STEP 5
+
+/*
+var firstName, lastName;
+firstName = "Leah";
+lastName = "Negrete";
+//or
+var = firstName
+var = lastName 
+firstName = "Leah";
+lastName = "Negrete";
+//or
+var firstName = "Leah", lastName = "Negrete";
+*/
+
+//STEP 6
+
+/*
+var a = 9 + "string";
+var b = (8 > 1) + "string";
+var c = (3 > 2) + 5;
+window.console.log(typeof a);
+window.console.log(typeof b);
+window.console.log(typeof c);
+/*
+
+//STEP 7
+
+/*
+money = "the root of all evil";
+var money;
+window.console.log(money);
+*/
+
+//STEP 8
+
+/*
+it's ok to use single or double quotes inside string as long they are not the same use \ to separate same quote marks within string - see correction below.
+*/
+
+/*
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+
+window.console.log(someString);
+*/
+
+//STEP 9
+
+/*
+var a = null;
+var b = undefined;
+window.console.log(a);
+window.console.log(b);
+*/
+
+//STEP 10
+
+/*
+var a = "string";
+var b = 2017;
+var c = true;
+var d = null;
+var none;
+window.console.log(typeof a);
+window.console.log(typeof b);
+window.console.log(typeof c);
+window.console.log(typeof d);
+window.console.log(typeof none);
+*/
+
+//STEP 11
+/*
+window.alert("Hello" + " " + "Leah Negrete" + "," + " " + "welcome to the Javascript class.");
+*/
+
+/*
+//STEP 12
+var name = "Leah Negrete";
+window.alert("Hello" + " " + name + "," + " " + "welcome to the Javascript class.");
+
+//STEP 13
+var course = "JavaScript";
+window.alert("Hello" + " " + name + "," + " " + "welcome to the" + " " + course + " " + "class.");
+
+//STEP 14
+window.alert("Hello" + " " + name + "," + "\n" + "welcome to the" + " " + course + " " + "class.");
+
+//STEP 15
+var yourname = prompt("Please enter your name.");
+window.alert("Hello" + " " + yourname + "," + "\n" + "welcome to the" + " " + course + " " + "class.");
+
+//STEP 16
+var yourname = prompt("Please enter your name.");
+var yourcourse = prompt("Pleae enter the course you are taking.")
+window.alert("Hello" + " " + yourname + "," + "\n" + "welcome to the" + " " + yourcourse + " " + "class.");
+*/
+
+//STEP 17
+
+/*
+var x = 10;
+var y = 20;
+window.console.log(x + y);
+*/
+
+//STEP 18
+
+/*
+var x = 20;
+x += 20;
+window.console.log(x);
+*/
+
+//STEP 19
+
+/*
+var x = 20;
+x *= 5;
+window.console.log(x);
+*/
+
+//STEP 20
+
+/*
+var x = 20 % 3;
+x /= 1;
+window.console.log(x);
+*/
+
+//STEP 21
+
+/*
+var x = 15, y = 3;
+window.console.log(x == y * 3 || x != y * 2);
+*/
+
+//STEP 22
+
+/*
+var x = 15, y = 3;
+window.console.log(x < y * 2 && y >= x - 3);
+*/
+
+//STEP 23
+
+/*
+var widget = new Object;
+window.console.log(typeof widget);
+*/
+
+//STEP 24
+
+/*
+var widget = new Object;
+window.console.log(widget instanceof Object);
+*/
+
+//STEP 25
+
+/*
+var widget = new Object;
+window.console.log(widget instanceof String);
+*/
